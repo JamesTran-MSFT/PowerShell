a2246e989ff61db414b8670541a70a52705bb464
James Tran - MSFT <36619112+JamesTran-MSFT@users.noreply.github.com>
2020-08-19 14:49:19 -0700

    Add files via upload

---
+++ Re-RequireMFA - AzureAD.ps1
@@ -0,0 +1,10 @@
+﻿#Connect to your Msol service
+Connect-MsolService -AzureEnvironment AzureCloud
+
+#This will get your user
+$User = Get-MSolUser -UserPrincipalName "user@domain.onmicrosoft.com"
+#This will have an output IF you have already completed MFA. If you haven't then this won't have any output.
+$User.StrongAuthenticationMethods
+
+#Resetting the strong authentication method. This will "Re-Require MFA" from the UPN you selected.
+Reset-MsolStrongAuthenticationMethodByUpn -UserPrincipalName "user@domain.onmicrosoft.com"
